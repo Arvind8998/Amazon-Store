a0b50e37c607e7dcba4526859b1b48060a30b0a5
arvind8998 <asangwan8998@gmail.com>
2021-01-03 13:38:03 +0530

    Amazon-store Architecture setup

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+          <div className="home__container">
+              <img className="home__image" alt="Knives Out amazon Banner" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg  "></img>
+            </div>  
+            <div className="home__row">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA084pxwrpRrpo6RieMDmauZlwlg8z3tAk",
+    authDomain: "store-937ab.firebaseapp.com",
+    projectId: "store-937ab",
+    storageBucket: "store-937ab.appspot.com",
+    messagingSenderId: "660010567559",
+    appId: "1:660010567559:web:ab8a53fba7ab31db573522",
+    measurementId: "G-GERKCPQ54B"
+  };
